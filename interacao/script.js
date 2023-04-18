e571e23ba8afb0ba774039af1a8d2945da689196
João Eduardo <49443445+JiangoLibre@users.noreply.github.com>
2023-04-18 18:18:27 -0300

    Update script.js
    Atualizado, botão de copiar agora copia texto cirptografado

--- interacao/script.js
+++ interacao/script.js
@@ -1,35 +1,34 @@
 var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output");
+var originalText = '';
 
-function criptografar(){
-    var texto = textInput.value;
+function criptografar() {
+    originalText = textInput.value;
 
-  var result1 = texto.replace(/e/g, "enter");
-  var result2 = result1.replace(/i/g, "imes");
-  var result3 = result2.replace(/a/g, "ai");
-  var result4 = result3.replace(/o/g, "ober");
-  var result5 = result4.replace(/u/g, "ufat");
-  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + result5 + 
-  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+    var result1 = originalText.replace(/e/g, "enter");
+    var result2 = result1.replace(/i/g, "imes");
+    var result3 = result2.replace(/a/g, "ai");
+    var result4 = result3.replace(/o/g, "ober");
+    var result5 = result4.replace(/u/g, "ufat");
+
+    outInput.innerHTML = '<textarea readonly id="input-texto">' + result5 +
+        '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function descriptografar(){
-    var texto = textInput.value;
-  
-    var result1 = texto.replace(/enter/g, "e");
+function descriptografar() {
+    var result1 = originalText.replace(/enter/g, "e");
     var result2 = result1.replace(/imes/g, "i");
     var result3 = result2.replace(/ai/g, "a");
     var result4 = result3.replace(/ober/g, "o");
     var result5 = result4.replace(/ufat/g, "u");
-  
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + result5 + 
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+
+    outInput.innerHTML = '<textarea readonly id="input-texto">' + result5 +
+        '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
-    var textoCop =  document.getElementById('input-texto');
-  
+    var textoCop = outInput.querySelector('textarea');
     textoCop.select();
     document.execCommand('copy');
     alert("Texto copiado para área de transferência.");
-}  
+}
